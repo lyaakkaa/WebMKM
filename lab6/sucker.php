b158014ed2691ffab60082dc91ae3dc73378125a
lyakusik <leila.bekturgan@gmail.com>
2023-10-26 23:31:38 +0600

    lab 6 done

--- lab6/sucker.php
+++ lab6/sucker.php
@@ -6,25 +6,87 @@
 </head>
 
 <body>
-<h1>Thanks, sucker!</h1>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $section = $_POST["section"];
+    $card = $_POST["cardNumber"];
+    $cardType = $_POST["card"];
 
-<p>Your information has been recorded.</p>
+    // Function to validate the credit card number using the Luhn Algorithm
+    function isLuhnValid($number) {
+        $number = str_replace('-', '', $number);
+        $sum = 0;
+        $isEven = false;
 
-<dl>
-    <dt>Name</dt>
-    <dd>
-        <?php echo $_POST['name']; ?>
-    </dd>
+        for ($i = strlen($number) - 1; $i >= 0; $i--) {
+            $digit = (int) $number[$i];
+            if ($isEven) {
+                $digit *= 2;
+                if ($digit > 9) {
+                    $digit -= 9;
+                }
+            }
+            $sum += $digit;
+            $isEven = !$isEven;
+        }
 
-    <dt>Section</dt>
-    <dd>
-        <?php echo $_POST['section']; ?>
-    </dd>
+        return ($sum % 10) === 0;
+    }
 
-    <dt>Credit Card</dt>
-    <dd>
-        <?php echo $_POST['creditcard']; ?>
-    </dd>
-</dl>
+    $isCardValid = false;
+
+    if (preg_match('/^(?:\d{4}-?){3}\d{4}$/', $card)) {
+        if (($cardType == "Visa" && preg_match('/^4/', $card)) ||
+            ($cardType == "MasterCard" && preg_match('/^5/', $card))) {
+            // Check if the credit card number is Luhn valid
+            if (isLuhnValid($card)) {
+                $isCardValid = true;
+            }
+        }
+    }
+
+    if (isset($name) && isset($section) && isset($card) && !empty($name) && !empty($section) && !empty($card) && $isCardValid) {
+        echo "<h1>Thanks, sucker!</h1>";
+
+        echo "<p>Your information has been recorded.</p>";
+
+        echo "<dl>
+                        <dt>Name</dt>
+                        <dd>" . $name . "</dd>
+
+                        <dt>Section</dt>
+                        <dd>" . $section . "</dd>
+
+                        <dt>Credit Card</dt>
+                        <dd>" . $card . "</dd>
+                        </dl>
+                
+                    <p>Here are all the suckers who have submitted here:</p>";
+
+        $file = 'suckers.txt';
+        $current = json_encode($_POST);
+        file_put_contents($file, $current . "\n", FILE_APPEND);
+
+        $data = file_get_contents("suckers.txt");
+        $jsonData = explode("\n", trim($data));
+
+        echo "<h1>Here are all the suckers who have submitted:</h1>";
+        foreach ($jsonData as $entry) {
+            $entryData = json_decode($entry, true);
+            if (isset($entryData['name']) && isset($entryData['section']) && isset($entryData['cardNumber']) && isset($entryData['card'])) {
+                $formattedData = $entryData['name'] . ";" . $entryData['section'] . ";" . $entryData['cardNumber'] . ";" . $entryData['card'];
+                echo $formattedData . "<br />";
+            }
+        }
+    } elseif (!$isCardValid) {
+        echo "<p>You provided an invalid card number or card type.</p>";
+        echo '<a href="buyagrade.html">Try again.</a>';
+    } else {
+        echo "<h1>Sorry</h1>";
+        echo '<a href="buyagrade.html">Try again.</a>';
+    }
+}
+?>
 </body>
 </html>
