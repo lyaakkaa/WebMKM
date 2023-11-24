9da8a9089a374c6bacb5df1e6fac6f41562ecd94
lyaakkaa <leila.bekturgan@gmail.com>
2023-11-24 12:13:54 +0600

    done lab10

--- lab10/task1/main.js
+++ lab10/task1/main.js
@@ -1,10 +1,21 @@
 const loadingContainer = document.getElementById('loading-container');
+const loadingSpinner = document.getElementById('loading-spinner');
+
 loadingContainer.style.display = 'flex';
 
-const updateInterval = 100; 
-let rotation = 0;
+const totalRotation = 360;
+let currentRotation = 0;
 
-setInterval(() => {
-    rotation += 10;
-    document.getElementById('loading-spinner').style.transform = `rotate(${rotation}deg)`;
-}, updateInterval);
+const rotateSpinner = () => {
+    currentRotation += 10;
+
+    if (currentRotation < totalRotation) {
+        loadingSpinner.style.transform = `rotate(${currentRotation}deg)`;
+        requestAnimationFrame(rotateSpinner);
+    } else {
+        loadingSpinner.style.transform = `rotate(${totalRotation}deg)`;
+        loadingContainer.innerHTML = '<p>Loaded!</p>';
+    }
+};
+
+requestAnimationFrame(rotateSpinner);
